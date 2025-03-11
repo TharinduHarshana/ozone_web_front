763bc8c8348914a6464d15946f78814e03f71c2b
TharinduHarshana <127109391+TharinduHarshana@users.noreply.github.com>
2025-03-11 10:16:06 +0530

    product listing change

--- src/pages/shopping-view/listing.jsx
+++ src/pages/shopping-view/listing.jsx
@@ -131,7 +131,7 @@ function ShoppingListing() {
   }, [filters]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-20 ">
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-5 ">
       <ProductFilter filters={filters} handleFilter={handleFilter} />
       <div className="bg-background w-full rounded-lg shadow-sm py-9">
         <div className="p-4 border-b flex items-center justify-between">
